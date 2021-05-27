5153ffe52ba11138fa1fea96105f9fd3149b05d9
Valentin <fritz.valentin@live.fr>
2021-05-27 23:02:55 +0200

    Fix tests

--- src/Restriktor.Tests/Core/MethodModelTests.cs
+++ src/Restriktor.Tests/Core/MethodModelTests.cs
@@ -7,12 +7,12 @@ namespace Restriktor.Tests.Core
     public class MethodModelTests
     {
         [Theory]
-        [InlineData("Console.WriteLine()", "Console.WriteLine()")]
+        [InlineData("Console.WriteLine()", "<global namespace>.Console.WriteLine()")]
         [InlineData("System.Console.WriteLine()", "System.Console.WriteLine()")]
-        [InlineData("Console.WriteLine(*)", "Console.WriteLine(*)")]
-        [InlineData("Console.WriteLine(System.Object)", "Console.WriteLine(System.Object)")]
-        [InlineData("Console.WriteLine(System.Object,System.String)", "Console.WriteLine(System.Object,System.String)")]
-        [InlineData("Console.WriteLine(System.Object, System.String )", "Console.WriteLine(System.Object,System.String)")]
+        [InlineData("System.Console.WriteLine(*)", "System.Console.WriteLine(*)")]
+        [InlineData("System.Console.WriteLine(System.Object)", "System.Console.WriteLine(System.Object)")]
+        [InlineData("System.Console.WriteLine(System.Object,System.String)", "System.Console.WriteLine(System.Object,System.String)")]
+        [InlineData("System.Console.WriteLine(System.Object, System.String )", "System.Console.WriteLine(System.Object,System.String)")]
         public void Parse_ToString(string method, string expected)
         {
             Check.That(MethodModel.Parse(method).ToString()).HasSameValueAs(expected);
